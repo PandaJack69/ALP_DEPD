a93297967493e6abf03a7910779184311f63042b
Chizumi <okdelvin8@gmail.com>
2025-12-16 00:04:07 +0700

    RegisterForn

---
+++ lib/view/pages/registrationpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../widgets/registrationformcard.dart';
+
+class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Memanggil class RegistrationForm dari registrationformcard.dart
+    return const RegistrationForm();
+  }
+}
